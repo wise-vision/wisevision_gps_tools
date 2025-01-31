3d9a992bf1013134969a76217f4dfaafd826c7c6
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-01-31 17:49:41 +0100

    Add license (#6)

--- src/GpsDeviceManager.cpp
+++ src/GpsDeviceManager.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2025 wisevision
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include "wisevision_gps_tools/GpsDeviceManager.hpp"
 
 GpsDeviceManager::GpsDeviceManager() : Node("gps_device_manager"), m_yaml_file("gps_devices.yaml") {
